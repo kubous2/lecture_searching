f71be9e93f1a58b351173f16364910bdfffd36b3
Jakub Rehurek <jakub.rehurek@seznam.cz>
2024-04-08 12:26:04 +0200

    "loading"

--- searching.py
+++ searching.py
@@ -1,4 +1,5 @@
 import os
+import json
 
 # get current working directory path
 cwd_path = os.getcwd()
@@ -13,9 +14,21 @@ def read_data(file_name, field):
     """
     file_path = os.path.join(cwd_path, file_name)
 
+    #načtení klíčů a ověření zda je klíč reálný
+    with open(file_name, mode = "r") as f:
+        allowed_keys = json.load(f)
+    if field not in allowed_keys:
+        return None
+
+    # načtení dat
+    with open(file_path, "r") as f:
+        data = json.load(f)
+    return data.get(field, None)
+
 
 def main():
-    pass
+    data = read_data("sequential.json", "unordered_numbers")
+    print(data)
 
 
 if __name__ == '__main__':
